95a0ab85c7d22b0e48a027d61a30e60592150cfa
Ayka <aykayao.dev@gmail.com>
2025-09-19 06:49:06 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import TodoList from './to-do-list'
+import './App.css'
+
+function App() {
+  return (
+    <TodoList/>
+  )
+}
+
+export default App
---
+++ src/to-do-list.jsx
@@ -0,0 +1,6 @@
+import React,{useState} from "react"
+function TodoList(){
+    const [task, setTask] = useState([]);
+    return(<></>)
+}
+export default TodoList
