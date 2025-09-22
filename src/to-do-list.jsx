e7de975ffe3d191e60f9dbd57af2e59fa4976880
LighT of HeaveN <jeric.ggonzales.dev@gmail.com>
2025-09-22 20:51:41 +0800

    commit 22

--- src/to-do-list.jsx
+++ src/to-do-list.jsx
@@ -1,7 +1,39 @@
 import React,{useState} from "react"
 function TodoList(){
     const [task, setTask] = useState([]);
-    return(<></>)
+    const [newtask, setNewTask] = useState("")
+
+    function handleInputChange(event){
+        setNewTask(event.target.value)
+
+    }
+    function addTask(){
+
+    }
+    function deleteTask(index){
+
+    }
+    function importantTask(index){
+
+    }
+    function lessImportantTask(index){
+
+    }
+    return(
+        <div>
+            <h1>To-do List Website by David Goggins</h1>
+        <div className="input-section">
+            <input type="text" placeholder="Enter a task..." value={newtask} onChange={handleInputChange} />
+            <button className="btn-add">Add Task</button>
+        </div>
+        <div>
+            <ul>
+                <li>
+                    <h1></h1>
+                </li>
+            </ul>
+        </div>
+    </div>)
 }
 export default TodoList
 
